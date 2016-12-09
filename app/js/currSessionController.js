df52d2b752f7df6eb4696ed4fcc70d6895179b5c
josediaz30 <jose.diaz30@upr.edu>
2016-12-08 22:03:16 -0400

    Added a controller

---
+++ app/js/currSessionController.js
@@ -0,0 +1,6 @@
+
+//var app = angular.module('what2eatApp', ['ui.bootstrap']); // include dependency on ui.boostrap
+
+app.controller('currSessionController', function foodListController($scope, $http){
+
+});
