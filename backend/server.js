8f949e00456545596e5f5e62eca58c4ca29c265f
josediaz30 <jose.diaz30@upr.edu>
2016-12-23 14:52:15 -0400

    Using rocket operator form es6

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.set('views', ngAppPath);
 require(__dirname + '/controller/api-routes.js')(app, version);
 
 // Start server
-const server = app.listen(port, function() {
+const server = app.listen(port, () => {
     console.log('Server started on port: ' + port);
 });
 
